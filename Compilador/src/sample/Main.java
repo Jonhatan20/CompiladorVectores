0ca424abb5242deaa92b600b16871bc7bc901c14
Jonhatan20 <jonyvilla44@gmail.com>
2018-10-04 19:44:52 -0600

    Agregamos editor

---
+++ Compilador/src/sample/Main.java
@@ -0,0 +1,37 @@
+package sample;
+
+import com.sun.javafx.application.LauncherImpl;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+    public static int duracion = 50000;
+    public static int steps = 1;
+
+    @Override
+    public void init() throws Exception {
+        for (int i = 0; i < duracion; i++) {
+            double progreso = (100 * i) / duracion;
+            LauncherImpl.notifyPreloader(this, new Splash.ProgressNotification(progreso));
+
+        }//LLAVE FOR
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Principal.fxml"));
+        primaryStage.setTitle("Compilador");
+        primaryStage.setScene(new Scene(root, 970, 700));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+
+        LauncherImpl.launchApplication(Main.class, Splash.class, args);
+    }
+}
